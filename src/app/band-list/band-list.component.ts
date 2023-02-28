12e8d000e5106cfbefb7785c799e4b8fda4c6599
Petros Tsavdaridis <peteropon@gmail.com>
2023-02-28 21:14:17 +0100

    Created files for band-list component.

---
+++ src/app/band-list/band-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'va-band-list',
+  templateUrl: './band-list.component.html',
+  styleUrls: ['./band-list.component.css']
+})
+export class BandListComponent {
+
+}
