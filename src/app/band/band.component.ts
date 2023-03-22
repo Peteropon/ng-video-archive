d914397d0e5493a350ff3fa42129c731935efbc4
Petros Tsavdaridis <peteropon@gmail.com>
2023-03-22 22:43:37 +0100

    Implemented service and back button on band comp.

--- src/app/band/band.component.ts
+++ src/app/band/band.component.ts
@@ -1,22 +1,34 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { IBand } from '../band-list/band';
+import { BandService } from '../band-list/band-list.service';
 
 @Component({
   selector: 'va-band',
   templateUrl: './band.component.html',
-  styleUrls: ['./band.component.css']
+  styleUrls: ['./band.component.css'],
 })
 export class BandComponent implements OnInit {
-  @Input() band: IBand = {
+  band?: IBand = {
     bandName: '',
     path: '',
-    videos: []
-  }
+    videos: [],
+  };
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(
+    private bandService: BandService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
-    const bandname = this.route.snapshot.paramMap.get('path')
+    const path = this.route.snapshot.paramMap.get('path');
+    if (path) {
+      this.band = this.bandService.getBand(path);
+    }
+  }
+
+  onBack(): void {
+    this.router.navigate(['']);
   }
 }
