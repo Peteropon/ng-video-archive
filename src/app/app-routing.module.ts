081f157be182586ee2994a737bd62f2bb9c5aa47
Petros Tsavdaridis <peteropon@gmail.com>
2023-07-17 22:27:02 +0200

    Created landing page comp and related files + setup.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,10 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { BandListComponent } from './band-list/band-list.component';
 import { BandComponent } from './band/band.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 const routes: Routes = [
+  { path: '', component: LandingPageComponent },
   { path: 'bands', component: BandListComponent, pathMatch: 'full' },
   { path: 'bands/:path', component: BandComponent },
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppComponent } from './app.component';
 import { BandListComponent } from './band-list/band-list.component';
 import { BandComponent } from './band/band.component';
 import { AppRoutingModule } from './app-routing.module';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 @NgModule({
-  declarations: [AppComponent, BandListComponent, BandComponent],
+  declarations: [AppComponent, BandListComponent, BandComponent, LandingPageComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
