d9e9dbf73be7541560bf9a9edc597e7660b2d702
Petros Tsavdaridis <peteropon@gmail.com>
2023-07-19 22:50:20 +0200

    Installed material angular.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,12 @@ import { BandListComponent } from './band-list/band-list.component';
 import { BandComponent } from './band/band.component';
 import { AppRoutingModule } from './app-routing.module';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { VideoDialogComponent } from './video-dialog/video-dialog.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [AppComponent, BandListComponent, BandComponent, LandingPageComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [AppComponent, BandListComponent, BandComponent, LandingPageComponent, VideoDialogComponent],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
