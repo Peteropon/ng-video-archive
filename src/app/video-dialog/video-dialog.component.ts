9c156604896482d4a6594aa08dc46186e6130d92
Petros Tsavdaridis <peteropon@gmail.com>
2023-07-19 22:48:54 +0200

    Created dialog comp for video player.

---
+++ src/app/video-dialog/video-dialog.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'va-video-dialog',
+  templateUrl: './video-dialog.component.html',
+  styleUrls: ['./video-dialog.component.css']
+})
+export class VideoDialogComponent {
+
+}
